d3df619579d1d4ac6c799c07b931592c2d2b36ab
17-jay <zhelejay3@gmail.com>
2025-02-22 19:09:44 +0300

    acronym creator

---
+++ acronyms.py
@@ -0,0 +1,10 @@
+
+user_input = str(input("Enter a Phrase: "))
+text = user_input.split()
+a = " "
+for i in text:
+    a = a+str(i[0]).upper()
+print(a)
+
+# split() splits a sentence into individual words
+# splitting a word to letters would require using list()
